039546983af1a55ad2b3815b8c0960a86de6537b
Tankerxyz <tankerxyz1@gmail.com>
2018-12-17 22:36:20 +0200

    add temporary controls for mobile-phones

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,38 @@
 import './styles/index.scss';
 import Game from './components/Game';
+import keycode from 'keycode'
 
 window.addEventListener('DOMContentLoaded', () => {
   const game = new Game('#renderCanvas');
+  const engine = document.querySelector('#renderCanvas');
+
+  // todo remove this fucking shitty code
+  const left = document.querySelector('.key.left');
+  const right = document.querySelector('.key.right');
+  const up = document.querySelector('.key.up');
+  const down = document.querySelector('.key.down');
+
+  left!.addEventListener('click', (e) => {
+    engine!.dispatchEvent(new Event('focus'));
+    // @ts-ignore
+    engine.dispatchEvent(new KeyboardEvent('keydown',{ keyCode: keycode.codes.left }))
+  });
+
+  right!.addEventListener('click', (e) => {
+    engine!.dispatchEvent(new Event('focus'));
+    // @ts-ignore
+    engine.dispatchEvent(new KeyboardEvent('keydown',{ keyCode: keycode.codes.right }))
+  });
+
+  up!.addEventListener('click', (e) => {
+    engine!.dispatchEvent(new Event('focus'));
+    // @ts-ignore
+    engine.dispatchEvent(new KeyboardEvent('keydown',{ keyCode: keycode.codes.up }))
+  });
+
+  down!.addEventListener('click', (e) => {
+    engine!.dispatchEvent(new Event('focus'));
+    // @ts-ignore
+    engine.dispatchEvent(new KeyboardEvent('keydown',{ keyCode: keycode.codes.down }))
+  });
 });
